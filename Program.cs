9c45b2feb3ba88eb03b69e16c50978ffb9dda4f6
Anton Vorontsov <antvork@gmail.com>
2023-02-23 20:40:46 +0700

    Добавлено описание и рещение

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
-﻿Console.WriteLine("Введите число: ");
-int N = Console.ReadLine();
-int a = 1;
-while(a < N)
-{
-    if(a%2==0)
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+ int a = 1;
+ while (a <= N)
     {
-        Console.Write(a);
-
+        if (a % 2 == 0)
+            {
+                Console.Write(a);
+                Console.Write(" ");
+            }
+            a = a + 1;
     }
-    a++;
-}
+    
